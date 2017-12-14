40e3accfd4eb51027aaabac6a97658541ef22c08
Dan <Daniel.Kemp@especiallyrelative.net>
2017-12-13 22:41:08 -0500

    Create config_example.js
    add config example

---
+++ configuration/config_example.js
@@ -0,0 +1,19 @@
+var config = {
+    firebase: {
+        apiKey: "",
+        authDomain: "",
+        databaseURL: "",
+        storageBucket: ""
+    },
+    paypal: {
+        mode: "",
+        clientId: "",
+        client_secret: ""
+    },
+    nodemail: {
+        email: "",
+        pass: ""
+    }
+}
+
+module.exports = config
